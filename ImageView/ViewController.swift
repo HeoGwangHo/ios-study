d011d343c75ae4d5836edd806e48bc2060b70842
HeoGwangHo <gjrhkdgh3@naver.com>
2020-01-28 18:55:38 +0900

    이미지뷰

---
+++ ImageView/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  2.ImageView
+//
+//  Created by macbook on 22/01/2020.
+//  Copyright © 2020 Gwang-Ho Heo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var isZoom = false // 이미디 확대 여부
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
+    }
+
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat = 2.0 // 확대한 배율 값
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if (isZoom) { // true 현재 확대된 그림일 경우
+            // 이미지 뷰의 프레임을 2.0으로 나눔
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+        }
+        else { // false 현재 축소된 그림일 경우
+            // 이미지 뷰의 프레임을 2.0으로 곱함
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
+    }
+    
+}
+
